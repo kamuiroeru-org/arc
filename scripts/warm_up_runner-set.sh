9149fe7c820949f2413836883e5d8499c1e03f26
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2024-08-13 21:01:12 +0900

    [bugfix]

--- scripts/warm_up_runner-set.sh
+++ scripts/warm_up_runner-set.sh
@@ -4,6 +4,6 @@ source settings.sh
 # Warm up 2 runners default.
 WARM_UP_RUNNER_COUNT=${WARM_UP_RUNNER_COUNT:-2}
 
-helm -n "${ARC_RUNNER_NAMESPACE}" upgrade --reuse-values "${INSTALLATION_NAME}" \
+helm -n "${ARC_RUNNER_NAMESPACE}" upgrade --reuse-values "${ARC_RUNNER_HELM_INSTALLATION_NAME}" \
   --set minRunners=$WARM_UP_RUNNER_COUNT \
   "${ARC_RUNNER_OCI_CHART}"
